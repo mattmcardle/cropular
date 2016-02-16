8bddb8f77050bbb17fd3599edd9bd5d255f371c9
Matt <matt@goreport.net>
2016-02-16 12:08:59 +0000

    add opensource image

--- example/demo.js
+++ example/demo.js
@@ -5,5 +5,5 @@ var app = angular.module('demo', ['ngSanitize', 'cropular']);
 app.controller('DemoCtrl', function($scope, $http, $timeout, $interval) {
     
     var ctrl = this;
-    ctrl.url = "http://api.goreport:8081/image/1602/13/614/29/5D88253B-BAEE-521D-9735-3B6DE53A065C.jpg/800";
+    ctrl.url = "https://images.unsplash.com/photo-1430329429612-babb42f88673?crop=entropy&dpr=2&fit=crop&fm=jpg&h=725&ixjsv=2.1.0&ixlib=rb-0.3.5&q=50&w=1300";
 });
